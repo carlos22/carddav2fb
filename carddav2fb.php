80ab8ca86029adb0a9e3158a814e233bba3627de
Jens Maus <mail@jens-maus.de>
2014-08-14 21:58:05 +0200

    applied changes found @ http://www.ip-phone-forum.de/showthread.php?t=267477

--- carddav2fb.php
+++ carddav2fb.php
@@ -13,19 +13,26 @@
  * 
  * LICENCE (of this file): MIT
  * 
- * autor: Karl Glatz
+ * Autor: Karl Glatz
+ * Modified by Martin Rost
  */
 error_reporting(E_ALL);
 setlocale(LC_ALL, 'de_DE.UTF8');
 
 require_once('lib/CardDAV-PHP/carddav.php');
 require_once('lib/vCard-parser/vCard.php');
-require_once('lib/fritzbox_api_php/lib/fritzbox_api.class.php');
+require_once('lib/fritzbox_api_php/fritzbox_api.class.php');
 
-if(is_file('config.php')) {
-	require('config.php');
+if ($argc == 2) {
+	$config_file_name = $argv[1];
 } else {
-	print 'ERROR: No config.php found, please take a look at config.example.php and create a config.php file!'.PHP_EOL;
+	$config_file_name = 'config.php';
+}
+
+if(is_file($config_file_name)) {
+	require($config_file_name);
+} else {
+	print 'ERROR: No '.$config_file_name.' found, please take a look at config.example.php and create a '.$config_file_name.' file!'.PHP_EOL;
 	exit(1);
 }
 
@@ -37,7 +44,7 @@ $client = new CardDAV2FB($config);
 
 
 // read vcards from webdav
-print 'Get all entires from CardDAV server(s)... ';
+print 'Get all entries from CardDAV server(s)... ';
 $client->get_carddav_entries();
 print 'Done.'.PHP_EOL;
 
@@ -51,7 +58,7 @@ print 'Done.'.PHP_EOL;
 flush(); // in case this script runs by php-cgi
 
 // upload the xml file to the fritz box (CAUTION: this will overwrite all current entries in the phonebook!!)
-print 'Upload to fritzbox at '.$config['fritzbox_url'].'...';
+print 'Upload to fritzbox at '.$config['fritzbox_ip'].'...';
 $ul_msg = $client->upload_to_fb();
 print 'Done.'.PHP_EOL;
 print 'FritzBox: '.$ul_msg.PHP_EOL;
@@ -96,31 +103,71 @@ class CardDAV2FB {
 				
 				// name
 				$name_arr = $vcard_obj->n[0];
-				$name = $name_arr['FirstName']." ".$name_arr['LastName'];
+
+				$name = $this->_concat($name_arr['Prefixes'],$this->_concat($this->_concat($name_arr['FirstName'],$name_arr['AdditionalNames']),$name_arr['LastName']));
+
+				if ($vcard_obj->photo) {
+					$photo = str_replace(array('&',' ','ä','ö','ü','Ä','Ö','Ü','ß','á','à','ó','ò','ú','ù'),
+							     array('_','_','ae','oe','ue','Ae','Oe','Ue','ss','a','a','o','o','u','u'),$name);
+				} else {
+					$photo = '';
+				}
 				
 				$phone_no = array();
-				$c = 0;
-				foreach($vcard_obj->tel as $t) {
-					
-					// only 3 numbers supported by fritz.box
-					if($c > 2) break;
-					
-					//TODO: improve this (add prio and vcard allows types like: TYPE=home,work,cell - how to handle this?)
-					$typearr_lower = unserialize(strtolower(serialize($t['Type'])));
-					if (in_array("work", $typearr_lower)) {
-						$type = "work";
-					}
-					if (in_array("cell", $typearr_lower)) {
-						$type = "mobile";
-					}
-					if (in_array("home", $typearr_lower)) {
-						$type = "home";
-					}
-					
-					$phone_no[] =  array("type"=>$type, "value" => $this->_clear_phone_number($t['Value']));
-					$c++;
+				if ($vcard_obj->categories) {
+					$categories = $vcard_obj->categories[0];
+				} else {
+					$categories = array('');
 				}
-				$entries[] = array("realName" => $name, "telephony" => $phone_no);
+				if (in_array($this->config['group_vip'],$categories)) {
+					$vip = 1;
+				}
+				else {
+					$vip = 0;
+				}
+
+				if (array_key_exists('group_filter',$this->config)) {
+					$add_entry = 0;
+					foreach($this->config['group_filter'] as $group_filter) {
+						if (in_array($group_filter,$categories)) {
+							$add_entry = 1;
+							break;
+						}
+					}
+				} else {
+					$add_entry = 1;
+				}
+
+				if ($add_entry == 1) {
+					foreach($vcard_obj->tel as $t) {
+
+						$prio = 0;
+						if (empty($t['Type'])) {
+							$type = "mobile";
+							$phone_number = $t;
+						}
+						else {
+							$phone_number = $t['Value'];
+							$typearr_lower = unserialize(strtolower(serialize($t['Type'])));
+		                                        if (in_array("work", $typearr_lower)) {
+		                                                $type = "work";
+		                                        }
+							elseif (in_array("cell", $typearr_lower)) {
+								$type = "mobile";
+							}
+							elseif (in_array("home", $typearr_lower)) {
+								$type = "home";
+								$prio = 1;
+							}
+							else {
+								continue;
+							}
+						}
+
+						$phone_no[] =  array("type"=>$type, "prio"=>$prio, "value" => $this->_clear_phone_number($phone_number));
+					}
+					$entries[] = array("realName" => $name, "telephony" => $phone_no, "vip" => $vip, "photo" => $photo);
+				}
 			}
 					
 			
@@ -131,7 +178,8 @@ class CardDAV2FB {
 	}
 
 	private function _clear_phone_number($number) {
-		return preg_replace("/[^0-9]/", "", $number);
+		return preg_replace("/[^0-9+]/", "", $number);
+		// return $number;
 	}
 
 	public function build_fb_xml() {
@@ -140,24 +188,30 @@ class CardDAV2FB {
 			throw new Exception('No entries available! Call get_carddav_entries or set $this->entries manually!');
 		}
 		
-		// create FB XML
-		$root = new SimpleXMLElement('<?xml version="1.0" encoding="iso-8859-1"?><phonebooks><phonebook></phonebook></phonebooks>');
-		$pb = $root->phonebook;
+                // create FB XML
+                $root = new SimpleXMLElement('<?xml version="1.0" encoding="iso-8859-1"?><phonebooks><phonebook></phonebook></phonebooks>');
+                $pb = $root->phonebook;
+		$pb->addAttribute("name",$this->config['phonebook_name']);
 		
 		foreach($this->entries as $entry) {
-				
+			
 				$contact = $pb->addChild("contact");
-				$contact->addChild("category");
+				$contact->addChild("category", $entry['vip']);
 				$person = $contact->addChild("person");
 				$person->addChild("realName", $this->_convert_text($entry['realName']));
+				if (($entry['photo']) and (array_key_exists('usb_disk',$this->config))) {
+					$person->addChild("imageURL","file:///var/media/ftp/".$this->config['usb_disk']."/FRITZ/fonpix/".$entry['photo'].".jpg");
+				}
 				$telephony = $contact->addChild("telephony");
-				
+			
+				$id = 0;	
 				foreach($entry['telephony'] as $tel) {
 					$num = $telephony->addChild("number", $tel['value']);
 					$num->addAttribute("type", $tel['type']);
 					$num->addAttribute("vanity","");
-					//TODO: fill prio 
-					$num->addAttribute("prio", "");
+					$num->addAttribute("prio", $tel['prio']);
+					$num->addAttribute("id", $id);
+					$id++;
 				}
 				
 				$contact->addChild("services");
@@ -176,6 +230,20 @@ class CardDAV2FB {
 		
 		return $text;
 	}
+
+	public function _concat ($text1,$text2) {
+
+		if ($text1 == '') {
+			return $text2;
+		}
+		elseif ($text2 == '') {
+			return $text1;
+		}
+		else
+		{
+			return $text1." ".$text2;
+		}
+	}
 	
 	public function _parse_fb_result($text) {
 			preg_match("/\<h2\>([^\<]+)\<\/h2\>/", $text, $matches);
@@ -187,12 +255,23 @@ class CardDAV2FB {
 	}
 
 	public function upload_to_fb() {
+
+
+		if (array_key_exists('output_file',$this->config)) {
+			$output = fopen($this->config['output_file'], 'w');
+			if ($output) {
+				fwrite($output, $this->fbxml);
+				fclose($output);
+			}
+			return 0;
+		};
+
 		$msg = "";
 		try
 		{
-		  $fritz = new fritzbox_api($this->config['fritzbox_pw'], $this->config['fritzbox_url']);
+		  $fritz = new fritzbox_api($this->config['fritzbox_pw'],$this->config['fritzbox_user'],$this->config['fritzbox_ip']);
 		  $formfields = array(
-			'PhonebookId' => '0',
+			'PhonebookId' => $this->config['phonebook_number']
 		  );
 		  
 		  $filefileds = array('PhonebookImportFile' => array(
--- config.example.php
+++ config.example.php
@@ -1,22 +1,28 @@
 <?php
 
 // CONFIG
-$config['fritzbox_url'] = 'fritz.box';
-$config['fritzbox_port'] = 80;
-$config['fritzbox_pw'] = false;
+$config['fritzbox_ip'] = 'fritz.box';
+$config['fritzbox_user'] = 'fb_username';
+$config['fritzbox_pw'] = 'fb_password';
+$config['phonebook_number'] = '0';
+$config['phonebook_name'] = 'FBF_Name';
+//$config['output_file'] = '/media/usbdisk/share/phonebook.xml';
+//$config['group_filter'] = array('Arzt','Familie','Freunde','Friseur','Geschäftlich','Hotline','Notruf','Restaurant','Shops');
+$config['group_vip'] = 'VIP';
+//$config['usb_disk'] = 'USBDISK2-0-01';
 
-// multiple carddav adressbooks could be specified
+// multiple carddav adressbooks could be specified and will be merged together.
 
 // first
 $config['carddav'][0] = array(
-	'url' => 'https://example.com/carddav/user@domian.de/MyContacts', 
-	'user' => 'user@domain.de', 
-	'pw' => 'supersecret'
+	'url' => 'https://raspserver/owncloud/remote.php/carddav/addressbooks/fritzbox/fb_contacts', 
+	'user' => 'oc_username', 
+	'pw' => 'oc_password'
 );
 
 // second
 //$config['carddav'][1] = array(
-//	'url' => 'https://carddav.example.com/contacts', 
-//	'user' => 'user', 
-//	'pw' => 'password'
+//	'url' => 'https://raspserver/owncloud/remote.php/carddav/addressbooks/fritzbox/fb_contacts_second', 
+//	'user' => 'oc_username', 
+//	'pw' => 'oc_password'
 //);
