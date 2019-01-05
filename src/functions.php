dbb834bbe68fc76920b9e524b4593381b81d9617
Wolfram Esser <github@derwok.de>
2019-01-05 15:02:56 +0100

    Allow correct filtering even if multiple servers share the same group names (#77)

--- src/functions.php
+++ src/functions.php
@@ -95,7 +95,11 @@ function dissolveGroups (array $vcards): array
 
     foreach ($vcards as $key => $vcard) {          // separate iCloud groups
         if (isset($vcard->xabsmember)) {
-            $groups[$vcard->fullname] = $vcard->xabsmember;
+            if (array_key_exists($vcard->fullname, $groups)) {
+                $groups[$vcard->fullname] = array_merge($groups[$vcard->fullname], $vcard->xabsmember);
+            } else {
+                $groups[$vcard->fullname] = $vcard->xabsmember;
+            }
             unset($vcards[$key]);
             continue;
         }
